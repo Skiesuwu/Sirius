34352cca91b4472d4f0e39d005cfcda26175c856
Marvelco <153199278+MarvelcoDev@users.noreply.github.com>
2024-02-04 23:19:31 +0200

    realized had to be in ts

--- Refund.ts
+++ Refund.ts
@@ -1,27 +1,27 @@
-app.post("/fortnite/api/game/v2/profile/*/client/RefundMtxPurchase", verifyToken, async (req, res) => {
-    
+app.post("/fortnite/api/game/v2/profile/*/client/RefundMtxPurchase", 
     const profiles = await Profile.findOne({ accountId: req.user.accountId });
-
-    let profile = profiles.profiles[req.query.profileId];
+    let profile = profiles.profiles[req.query.profileId as string];
     let athena = profiles.profiles["athena"];
-
-
-    var ApplyProfileChanges = [];
-    let MultiUpdate = [{
+    let ApplyProfileChanges: { changeType: string; itemId?: string; name?: string; value?: any; profile?: any }[] = [];
+    let MultiUpdate: {
+        profileRevision: number;
+        profileId: string;
+        profileChangesBaseRevision: number;
+        profileChanges: any[];
+        profileCommandRevision: number;
+    }[] = [{
         "profileRevision": athena.rvn || 0,
         "profileId": "athena",
         "profileChangesBaseRevision": athena.rvn || 0,
         "profileChanges": [],
         "profileCommandRevision": athena.commandRevision || 0,
     }];
-    var BaseRevision = profile.rvn;
-    var QueryRevision = req.query.rvn || -1;
-    var StatChanged = false;
+    let BaseRevision = profile.rvn;
+    let QueryRevision = req.query.rvn || -1;
+    let StatChanged = false;
     let ProfileRevisionCheck = (memory.build >= 12.20) ? profile.commandRevision : profile.rvn;
-
-    var ItemCost = [];
-    var ItemGuids = [];
-
+    let ItemCost: any[] = [];
+    let ItemGuids: string[] = [];
     if (req.body.purchaseId) {
         MultiUpdate.push({
             "profileRevision": athena.rvn || 0,
@@ -30,62 +30,49 @@ app.post("/fortnite/api/game/v2/profile/*/client/RefundMtxPurchase", verifyToken
             "profileChanges": [],
             "profileCommandRevision": athena.commandRevision || 0,
         })
-
         profile.stats.attributes.mtx_purchase_history.refundsUsed += 1;
         profile.stats.attributes.mtx_purchase_history.refundCredits -= 1;
-
-        for (var i = 0; i < profile.stats.attributes.mtx_purchase_history.purchases.length; i++) {
+        for (let i = 0; i < profile.stats.attributes.mtx_purchase_history.purchases.length; i++) {
             if (profile.stats.attributes.mtx_purchase_history.purchases[i].purchaseId == req.body.purchaseId) {
-                for (var x = 0; x < profile.stats.attributes.mtx_purchase_history.purchases[i].lootResult.length; x++) {
+                for (let x = 0; x < profile.stats.attributes.mtx_purchase_history.purchases[i].lootResult.length; x++) {
                     ItemGuids.push(profile.stats.attributes.mtx_purchase_history.purchases[i].lootResult[x].itemGuid)
                 }
                 profile.stats.attributes.mtx_purchase_history.purchases[i].refundDate = new Date().toISOString();
             }
         }
-        for (var i = 0; i < ItemGuids.length; i++) {
+        for (let i = 0; i < ItemGuids.length; i++) {
             delete athena.items[ItemGuids[i]]
-
             MultiUpdate[0].profileChanges.push({
                 "changeType": "itemRemoved",
                 "itemId": ItemGuids[i]
             })
         }
-
         athena.rvn += 1;
         athena.commandRevision += 1;
         profile.rvn += 1;
         profile.commandRevision += 1;
-
         StatChanged = true;
     }
-
     if (StatChanged == true) {
         profile.updated = new Date().toISOString();
-
         await profile.updateOne({ $set: { [`profiles.${req.query.profileId}`]: profile } });
-
-
         ApplyProfileChanges.push({
             "changeType": "statModified",
             "name": "mtx_purchase_history",
             "value": profile.stats.attributes.mtx_purchase_history
         })
-
         MultiUpdate[0].profileRevision = athena.rvn || 0;
         MultiUpdate[0].profileCommandRevision = athena.commandRevision || 0;
     }
-
-    
-   if (QueryRevision != ProfileRevisionCheck) {
+    if (QueryRevision != ProfileRevisionCheck) {
         ApplyProfileChanges = [{
             "changeType": "fullProfileUpdate",
             "profile": profile
         }];
     }
-
     res.json({
         profileRevision: profile.rvn || 0,
-        profileId: req.query.profileId,
+        profileId: req.query.profileId as string,
         profileChangesBaseRevision: BaseRevision,
         profileChanges: ApplyProfileChanges,
         profileCommandRevision: profile.commandRevision || 0,
@@ -93,3 +80,4 @@ app.post("/fortnite/api/game/v2/profile/*/client/RefundMtxPurchase", verifyToken
         responseVersion: 1
     });
 });
+
